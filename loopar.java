813d73813a8eb7e843064f712cf49878f539c88f
Isac Hansson <isac.hansson@learnet.se>
2019-10-10 11:11:03 +0200

    torsdag

---
+++ loopar.java
@@ -0,0 +1,18 @@
+/*
+Uppgift: Test
+Skapat 2019-10-10
+Dev: Isac Hansson
+*/
+import javax.swing.*;
+
+public class loopar	{
+	public static void main (String[] args) {
+		double a = 0; 
+		double b = 10000000000.0; 
+		
+		while (a < b) {
+			System.out.print(a);
+			a++;
+		}
+	}
+}
